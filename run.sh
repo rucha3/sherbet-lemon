8dae2bfb4bd372f2529e7d47ebfee07b9202ec1a
Rucha <rucha@indix.com>
2018-09-21 17:46:58 +0530

    build version number

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ docker images
 echo $docker_usname1
 echo $docker_pswd1
 docker login -u $docker_usname1 --password $docker_pswd1
-docker push rucha3/sherbet-lemon
-nam=`docker run -d -p 8000:8000 rucha3/sherbet-lemon`
+docker push $img
+nam=`docker run -d -p 8000:8000 $img`
 docker ps -a
 sleep 5
 curl http://localhost:8000/health
