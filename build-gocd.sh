0efcabf675377fc2342ed0802daa5b9909da52e7
Rucha <rucha@indix.com>
2018-09-24 16:01:35 +0530

    docker permission

--- build-gocd.sh
+++ build-gocd.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 
+DOCKER_SOCKET=/var/run/docker.sock
+DOCKER_GROUP=docker
+BUILD_USER=go
+
+if [ -S ${DOCKER_SOCKET} ]; then
+    DOCKER_GID=$(stat -c '%g' ${DOCKER_SOCKET})
+
+    #addgroup is distribution specific
+
+    addgroup -S -g ${DOCKER_GID} ${DOCKER_GROUP}
+    addgroup  ${BUILD_USER} ${DOCKER_GROUP}
+fi
 sbt clean test assembly
 img=rucha3/sherbet-lemon-gocd-1.0."$GO_PIPELINE_COUNTER"
 docker build -t $img .
